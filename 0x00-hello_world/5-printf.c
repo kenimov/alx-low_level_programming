a3d357892fa17a06ebe67b9c660713b78ef99261
kenimov <kenimovpotter@gmail.com>
2023-09-11 02:56:46 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-  * main - Prints a text}
+  * main - Prints a text
   *
   * Return: Always 0 (Success)
   */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
